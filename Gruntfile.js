cd3235860d49697f8031307aabfbd09acfc47dce
Evgeny Rahman <evgeny.rahman@gmail.com>
2014-03-30 09:19:20 -0400

    Trying out load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,12 +81,7 @@ module.exports = function(grunt) {
   });
 
   //Register tasks
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  require('load-grunt-tasks')(grunt);
 
   // Default task
   grunt.registerTask('default', ['jshint', 'concat']);
